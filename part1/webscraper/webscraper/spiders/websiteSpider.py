d8076761b41b65133af81a47220367878cac6d05
Parit Kansal <132123353+ParitKansal@users.noreply.github.com>
2024-09-07 12:19:54 +0530

    Update websiteSpider.py

--- part1/webscraper/webscraper/spiders/websiteSpider.py
+++ part1/webscraper/webscraper/spiders/websiteSpider.py
@@ -3,10 +3,9 @@ from webscraper.items import WebscraperItem
 
 class WebsitespiderSpider(scrapy.Spider):
     name = "websiteSpider"
-    allowed_domains = ["books.toscrape.com", "seetickets.com"]
+    allowed_domains = ["www.amazon.in"]
     start_urls = [
-        "https://books.toscrape.com",
-        "https://www.seetickets.com",
+        "https://www.amazon.in/Nike-Mens-White-Black-White-Sneakers/dp/B098F462JJ/ref=sr_1_6?psc=1",
     ]
     
     custom_settings = {
@@ -22,8 +21,20 @@ class WebsitespiderSpider(scrapy.Spider):
         # Extract the body content as HTML
         body_html = response.xpath('//body').get()
         
-        # Assign the HTML content to the item
+        # Assign the HTML content and URL to the item
+        web_item['url'] = response.url
         web_item['body_html'] = body_html
         
         # Yield the item to the Scrapy pipeline
         yield web_item
+        
+        """
+        # Extract and follow links to other pages on the website
+        for href in response.xpath('//a/@href').extract():
+            # Build the absolute URL
+            url = response.urljoin(href)
+            # Check if the URL has a valid scheme
+            if url.startswith(('http://', 'https://')):
+                # Follow the link
+                yield scrapy.Request(url, callback=self.parse)
+        """
